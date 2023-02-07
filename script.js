791441acdb60ad2fc6024af1be0161971a2c7dc9
0x000552 <32146826+0x000552@users.noreply.github.com>
2023-02-07 21:11:04 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict";
+// let rawTelNumber = '+62 811-8951-181';
+
+let rawTelNumber = prompt('Enter Indonesian number');
+
+let arrayTelNumber = Array.from(rawTelNumber).reduce (
+  (prev, curr) => {
+    if ( isFinite( parseInt(curr) ) ) {
+      prev.push(curr)
+    }
+    return prev;
+  }, []
+)
+
+// remove leading z
+while ( arrayTelNumber.length > 1 && arrayTelNumber[0] === '0' ) {
+  arrayTelNumber = arrayTelNumber.slice(1)
+}
+
+if (arrayTelNumber.length <= 10) {
+  throw new Error(`Too short number ${arrayTelNumber}`);
+}
+
+if ( arrayTelNumber.slice(0,2).join('') !== '62' ) {
+  arrayTelNumber.unshift(6,2);
+}
+
+const telNumber = arrayTelNumber.join('');
+console.log(telNumber);
+
+document.body.innerHTML = `<p>Press me: <a href="https://wa.me/${telNumber}">wa.me/${telNumber}</a></p>`;
+
+//confirm() ? window.open(`https://wa.me/${telNumber}`) : null;
